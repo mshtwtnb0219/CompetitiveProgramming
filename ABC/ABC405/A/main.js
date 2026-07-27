700a51a161c92eb844c92d42ad6ac4dd7695d0c0
masa <b77yellow@gmail.com>
2026-07-27 16:48:02 +0900

    403～407 A

---
+++ ABC/ABC405/A/main.js
@@ -0,0 +1,78 @@
+// ===============================
+// 競技プログラミング用テンプレート
+// ===============================
+
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "./input.txt", "utf8")
+  .trim()
+  .split(/\r?\n/);
+
+let index = 0;
+
+// ===============================
+// 入力
+// ===============================
+
+// 1つの整数
+// const N = Number(input[index++]);
+
+// 1つの文字列
+// const S = input[index++];
+
+// スペース区切りの整数
+const [R, X] = input[index++].split(" ").map(Number);
+
+ans = false;
+if (X == 1) {
+  if (R >= 1600 && R <= 2999) {
+    ans = true;
+  }
+} else if (X == 2) {
+  if (R >= 1200 && R <= 2399) {
+    ans = true;
+  }
+}
+
+console.log(ans ? "Yes" : "No");
+
+// スペース区切りの文字列
+// const [A, B] = input[index++].split(" ").map(String);
+
+// N個の整数
+// const A = input[index++].split(" ").map(Number);
+
+// N行の整数
+// const A = [];
+// for (let i = 0; i < N; i++) {
+//   A.push(Number(input[index++]));
+// }
+
+// N行の文字列
+// const S = [];
+// for (let i = 0; i < N; i++) {
+//   S.push(input[index++]);
+// }
+
+// H×Wのグリッド
+// const grid = [];
+// for (let i = 0; i < H; i++) {
+//   grid.push(input[index++]);
+// }
+
+// H×Wの数値グリッド
+// const grid = [];
+// for (let i = 0; i < H; i++) {
+//   grid.push(input[index++].split(" ").map(Number));
+// }
+
+// ===============================
+// 処理
+// ===============================
+
+// ===============================
+// 出力
+// ===============================
+
+// console.log(ans);
