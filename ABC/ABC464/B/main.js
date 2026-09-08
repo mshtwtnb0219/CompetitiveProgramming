9be371100e2c6f9f398c9f195926ca03c5cf6cf1
masa <b77yellow@gmail.com>
2026-09-08 17:02:06 +0900

    464 B

--- ABC/ABC464/B/main.js
+++ ABC/ABC464/B/main.js
@@ -22,30 +22,41 @@ let index = 0;
 // const S = input[index++];
 
 // スペース区切りの整数
-const [A, B] = input[index++].split(" ").map(Number);
+let [H, W] = input[index++].split(" ").map(Number);
 
-// N行の文字列
-let S = [];
-for (let i = 0; i < A; i++) {
-  S.push(input[index++].split("").map(String));
+const C = [];
+for (let i = 0; i < H; i++) {
+  C.push(input[index++].split("").map(String));
 }
 
-countkomna = 0;
+// 最上行
+while (C[0].every((x) => x === ".")) {
+  C.splice(0, 1);
+}
 
-console.log(S[0][0]);
+// 最下行
+while (C[C.length - 1].every((x) => x === ".")) {
+  C.pop();
+}
 
-for (let i = 0; i < A; i++) {
-  for (let j = 0; j < B; j++) {
-    if ("." == S[i][j]) {
-      countkomna++;
-      if (countkomna == B) {
-        S.pop(i);
-      }
-    }
+// 最左列
+while (C.every((row) => row[0] === ".")) {
+  for (let i = 0; i < C.length; i++) {
+    C[i].splice(0, 1);
   }
 }
 
-console.log(S);
+// 最右列
+while (C.every((row) => row[row.length - 1] === ".")) {
+  for (let i = 0; i < C.length; i++) {
+    C[i].pop();
+  }
+}
+
+for (let i = 0; i < C.length; i++) {
+  console.log(C[i].join(""));
+}
+
 // スペース区切りの文字列
 // const [A, B] = input[index++].split(" ").map(String);
 
@@ -57,6 +68,8 @@ console.log(S);
 // for (let i = 0; i < N; i++) {
 //   A.push(Number(input[index++]));
 // }
+
+// N行の文字列
 
 // H×Wのグリッド
 // const grid = [];
@@ -88,6 +101,42 @@ console.log(S);
 //   const arr = str.match(/^[A-Z]+$/);
 //   return arr ? true : false;
 // };
+
+// num = String(N).split("");
+// 配列の要素の入れ替え　昇順
+// num.sort((a, b) => a.localeCompare(b));
+// console.log(num);
+// 配列の要素の入れ替え　降順
+// num.sort((a, b) => b.localeCompare(a));
+// console.log(num);
+
+// Number("001") // 1
+// Number("007") // 7
+// Number("010") // 10
+// Number("100") // 100
+
+// 10進数　→ N進数
+// (83).toString(2); // "1010011"  2進数
+// (83).toString(8); // "123"      8進数
+// (83).toString(10); // "83"       10進数
+// (83).toString(16); // "53"       16進数
+
+// 自力で10進数　→　N進数変換  ※これは8進数に変換
+// let N = Number(input[index++]);
+// result = "";
+
+// while (N > 0) {
+//   const remainder = N % 8; // 余り
+//   result = remainder + result;
+//   N = Math.floor(N / 8); // 商
+// }
+
+// const count = Array(N + 1).fill(0);
+
+// for (let i = 0; i < N; i++) {
+//   const num = A[0][i]; // 今見ている数字
+//   count[num]++;        // その数字の登場回数を+1
+// }
 
 // ===============================
 
